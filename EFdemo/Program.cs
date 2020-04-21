7cfdbba241679a34e841db6248881026b7f5faaa
suweicheng <suweicheng@yodoo.net.cn>
2020-04-21 16:51:38 +0800

    添加项目文件。

---
+++ EFdemo/Program.cs
@@ -0,0 +1,146 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace EFdemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //1.声明上下文
+
+            FirstDbEntities dbContext = new FirstDbEntities();
+
+            #region 新增操作
+
+
+
+            //2.声明一个学生实体；
+
+            //student stu = new student();
+
+            //stu.Sid = "0005";
+            //stu.Sname = "天猫";
+            //stu.Ssex = "男";
+            //stu.Sbirthdate = DateTime.Now.Date;
+
+            ////3.告诉ef对上面的实体进行一个插入操作
+
+            //dbContext.student.Add(stu);
+
+            ////4.告诉上下文，把实体的变化保存到数据库
+
+            //dbContext.SaveChanges();
+            #endregion
+
+            #region 修改操作
+
+            ////1.声明上下文
+
+
+            ////2.声明一个学生实体；
+
+            //student stu = new student();
+
+            //stu.Sid = "0005";//修改删除的时候 指定ID
+            //stu.Sname = "马天猫";
+            //stu.Ssex = "男";
+            //stu.Sbirthdate = DateTime.Now.Date;
+
+            ////3.告诉ef对上面的实体进行一个修改操作
+
+            ////dbContext.Entry<student>(stu).State = System.Data.Entity.EntityState.Modified;//实体全部修改
+            //dbContext.student.Attach(stu);//附加到上下文来管理
+
+            //dbContext.Entry<student>(stu).Property<string>(u => u.Sname).IsModified = true;//只修改某个列
+
+            ////4.告诉上下文，把实体的变化保存到数据库
+
+            //dbContext.SaveChanges();
+
+            #endregion
+
+            #region 查询操作
+
+
+            //1.把学生表所有数据打出来
+
+            //foreach (var item in dbContext.student)
+            //{
+            //    Console.WriteLine(item.Sid +" " +item.Sname);
+            //}
+
+            ////单独查询性别为男的 linq查询
+
+            //var temp = from u in dbContext.student
+
+            //           where u.Ssex == "男"
+
+            //           select u;
+
+            //var str = from it in dbContext.student
+
+            //          where it.Sname.Contains("子")
+
+            //          select it;
+
+            //foreach (var item in temp)
+            //{
+            //    Console.WriteLine(item.Sid + " " + item.Sname + " " + item.Ssex);
+            //}
+
+            //foreach (var item in str)
+            //{
+            //    Console.WriteLine(item.Sid + " " + item.Sname + " " + item.Ssex);
+            //}
+
+
+            List<int> list = new List<int>() {1,2,3 };
+
+            //List<object> listObj = list;
+
+            //foreach (var item in listObj)
+            //{
+
+            //}
+
+            IQueryable<student> stu = from u in dbContext.student
+                                      where u.Ssex == "女"
+                                      select u;
+
+            IQueryable<object> parent = stu;
+
+
+            //协变 。 把一个子类的泛型集合赋值给父类的泛型集合，外边用的时候 用父类泛型；
+
+
+            Func<int, bool> delFunc;
+
+            Func<object, bool> delParent = a => { Console.WriteLine(a.ToString());
+                                                    return true; };
+
+
+
+
+            //逆变  还是为了安全，把父类的泛型给子类的泛型对象
+
+            #endregion
+
+
+            #region 
+
+            #endregion
+
+
+
+            Console.ReadKey();
+
+
+
+
+        }
+    }
+}
---
+++ ModelFiestDemo/Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ModelFiestDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DataModelContainer dbContext = new DataModelContainer();
+
+
+
+            //#region MyRegion  新增
+            //Customer customer = new Customer();
+            //customer.CusName = "yang";
+            //dbContext.Customer.Add(customer);
+
+            //dbContext.SaveChanges();
+
+
+
+            //#endregion
+
+
+            #region 修改
+
+            Customer customer1 = new Customer();
+
+            customer1.CusName = "杨过";
+            customer1.Id = 1;
+
+            //3.告诉ef对上面的实体进行一个修改操作
+
+            //dbContext.Entry<student>(stu).State = System.Data.Entity.EntityState.Modified;//实体全部修改
+            dbContext.Customer.Attach(customer1);//附加到上下文来管理
+
+            dbContext.Entry<Customer>(customer1).Property<string>(u => u.CusName).IsModified = true;
+         
+
+            //4.告诉上下文，把实体的变化保存到数据库
+
+            dbContext.SaveChanges();
+
+
+
+            #endregion
+        }
+    }
+}
